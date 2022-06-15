be3800cb26af09407be03ace56b04ca1d5f9be3f
Rooolien <98538187+Rooolien@users.noreply.github.com>
2022-06-15 09:23:24 +0200

    Delete myKMeans.R

--- R/myKMeans.R
+++
@@ -1,9 +0,0 @@
-#'myKMeans: This package clusters data points into distinct non-overlapping clusters.
-#'
-#'This packages uses the euclidean distance between the k data points and the 
-#'means of the clusters (centroids).
-#'
-#'@section myKMeans
-#'@docType package
-#'@name myKMeans
-NULL
